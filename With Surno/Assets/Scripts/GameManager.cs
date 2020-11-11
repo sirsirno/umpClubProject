0e5e7866d31f25832ccc03b2a55617d3531b14bc
KimMinSang <minsang1008@naver.com>
2020-11-11 17:45:01 +0900

    자자 맵을 만듭시다 선호 나는 플레이어 움직임과 상호작용 카메라설정을 끝 마쳤으니까

---
+++ With Surno/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text talkText;
+    public bool isAction = false;
+    [SerializeField]
+    private GameObject talkP;
+  
+    public void ReAction()
+    {
+        if (isAction)//exit Action
+        {
+            isAction = false;
+        }
+        else
+        {
+            isAction = true;
+            
+        }
+       
+    }
+    public void Contact(RaycastHit scanObj)
+    {
+        if (scanObj.transform.CompareTag("Object"))
+        {
+            if(scanObj.transform.name == "Capsule")
+            {
+                talkText.text = "이것은 캡슐";
+            }
+            if(scanObj.transform.name == "Book")
+            {
+                talkText.text = "아무튼 책임";
+            }
+            StartCoroutine("Panel");
+        }
+        else
+        {
+            NotContact();
+        }
+    }
+    private void NotContact()
+    {
+        Debug.Log("아무튼 아니야");
+
+    }
+    IEnumerator Panel()
+    {
+        talkP.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        talkP.SetActive(false);
+    }
+}
---
+++ With Surno/Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+    [SerializeField]
+    private float runSpeed = 1.5f;
+
+    private float stamina = 100f;
+    private float maxStamina;
+
+    private bool isRun = false;
+
+    Rigidbody rigid = null;
+    [SerializeField]
+    private GameManager manager;
+
+    private GameObject scanObject;
+
+    private RaycastHit actionInfo;
+
+    [SerializeField]
+    private Camera theCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+        maxStamina = stamina;
+    }
+
+    private void Update()
+    {
+        Action();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        Run();
+    }
+   
+    private void Move()
+    {
+        //이동
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+            moveVelocity = transform.TransformDirection(Vector3.back);
+        else if (Input.GetAxisRaw("Vertical") > 0)
+            moveVelocity = transform.TransformDirection(Vector3.forward);
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            moveVelocity = transform.TransformDirection(Vector3.left);
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+            moveVelocity = transform.TransformDirection(Vector3.right);
+        transform.position += moveVelocity * speed * Time.deltaTime;
+       
+    }
+    private void Run()
+    {
+        //달리기
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            speed = runSpeed;
+            isRun = true;
+            stamina--;
+        }
+        if (stamina < maxStamina && isRun == false)
+            stamina += .5f;
+        if (!Input.GetKey(KeyCode.LeftShift) || stamina <= 0)
+        {
+            isRun = false;
+            speed = moveSpeed;
+        }
+        stamina += 0.1f * Time.deltaTime;
+    }
+    private void RayCast()
+    {
+        //Ray
+        Debug.DrawRay(theCam.transform.position, theCam.transform.forward, new Color(0, 1, 0));
+        if (Physics.Raycast(theCam.transform.position, theCam.transform.forward, out actionInfo, 100))
+        {
+            manager.Contact(actionInfo);
+        }
+    }
+    private void Action()
+    {
+        //상호작용
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            RayCast();
+            manager.ReAction();
+        }
+    }
+}
